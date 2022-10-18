59e5f20276dd247f666aaa3eea06ee111e557166
Eldar Safiev <eldar.safiev.97@mail.ru>
2022-10-18 18:25:56 +0300

    First commit

---
+++ Exercises/src/Sound.java
@@ -0,0 +1,7 @@
+public class Sound {
+    public static void main(String[] args) {
+        double dist;
+        dist = 1100 * 7.2;
+        System.out.println("Расстрояние до места вспышки молнии составляет " + dist + " футов" );
+    }
+}
