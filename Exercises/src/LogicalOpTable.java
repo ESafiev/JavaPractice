6629217f58a9143bfb9acd9109d067588b69aefa
Eldar Safiev <eldar.safiev.97@mail.ru>
2022-10-19 19:30:30 +0300

    LogicalOpTable_v2

--- Exercises/src/LogicalOpTable.java
+++ Exercises/src/LogicalOpTable.java
@@ -5,23 +5,23 @@ public class LogicalOpTable {
         System.out.println("P\tQ\tAND\tOR\tXOP\tNOT");
 
         p = true; q = true;
-        System.out.println(p+"\t"+q+"\t");
-        System.out.println((p&q) + "\t" + (p|q) + "\t");
+        System.out.print(p+"\t"+q+"\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
         System.out.println((p^q) + "\t" + (!p));
 
         p = true; q = false;
-        System.out.println(p+"\t"+q+"\t");
-        System.out.println((p&q) + "\t" + (p|q) + "\t");
+        System.out.print(p+"\t"+q+"\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
         System.out.println((p^q) + "\t" + (!p));
 
         p = false; q = true;
-        System.out.println(p+"\t"+q+"\t");
-        System.out.println((p&q) + "\t" + (p|q) + "\t");
+        System.out.print(p+"\t"+q+"\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
         System.out.println((p^q) + "\t" + (!p));
 
         p = false; q = false;
-        System.out.println(p+"\t"+q+"\t");
-        System.out.println((p&q) + "\t" + (p|q) + "\t");
+        System.out.print(p+"\t"+q+"\t");
+        System.out.print((p&q) + "\t" + (p|q) + "\t");
         System.out.println((p^q) + "\t" + (!p));
 
     }
