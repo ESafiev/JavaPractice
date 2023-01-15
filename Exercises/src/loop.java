05c5c82e8ed15cd50fe65070e12e6f826f18b7e8
Eldar Safiev <eldar.safiev.97@mail.ru>
2023-01-15 20:31:29 +0300

    loop added

---
+++ Exercises/src/loop.java
@@ -0,0 +1,12 @@
+public class loop {
+
+    public static void main(String[] args) {
+        int i = 0;
+        while (i <= 1000) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
